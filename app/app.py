0ed08e6d82ff7661abb88949c33f646d64c2f1ba
GANGUMALLA VENKATESH <87870265+VENKATESH2486@users.noreply.github.com>
2023-09-29 12:47:42 +0530

    change3

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'everyone Good Afternoon!Be always Happy'
+    return 'Good Afternoon!Be always Happy'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
